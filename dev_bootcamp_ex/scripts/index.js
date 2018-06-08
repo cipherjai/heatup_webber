ab78f4a1ec78eda4ec95699ed67d5e477a7486e3
cipherJai <jaiatmart@gmail.com>
2018-06-08 13:22:27 +0530

    for new files

---
+++ dev_bootcamp_ex/scripts/index.js
@@ -0,0 +1,12 @@
+console.log("The Beatles".length);
+console.log("The Beatles"[10] );
+console.log("Somebody".length % "No".length);
+console.log("Somebody\\".length);
+
+// returns whats remaining
+
+// Null and undefined
+var name;
+var age;
+console.log(name);
+console.log(age);
